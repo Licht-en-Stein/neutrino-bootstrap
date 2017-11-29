517e7e73a5fa996aae087890cc1f56918dbffa88
Licht-en-Stein <boimte@t-online.de>
2017-11-29 15:09:33 +0100

    changed the navbar names, using the categorie names to fill

--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ $(() => {
       const $carousel = mkCarousel(categories);
       $('#root').append($carousel);
       $carousel.carousel();
+      $('.navbar-nav').empty();
+      for (let i = 0; i < categories.length; i += 1) {
+        $('.navbar-nav').append(`
+        <li class="nav-item">
+        <a class="nav-link" href="#">${categories[i].name}<span class="sr-only">(current)</span></a>
+      </li>
+        `);
+      }
     });
   $('#root').append(navbarTemplate);
 });
