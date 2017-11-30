b65561da535916ab84f581de008a2123799c4caa
Licht-en-Stein <boimte@t-online.de>
2017-11-30 14:26:24 +0100

    added card grid to index.js, added margin to the card in styles.scss

--- src/index.js
+++ src/index.js
@@ -3,8 +3,29 @@ import 'bootstrap/js/src';
 import './styles.scss';
 import navbarTemplate from './templates/navbar.html';
 import mkCarousel from './carousel';
+import productCardTemplate from './templates/product-card.html';
+
+function mkProductCard(product) {
+  const $card = $(productCardTemplate);
+  $card.find('.card-title').text(product.name);
+  $card.find('.card-img-top').attr('src', `http://via.placeholder.com/360x240?text=${product.name}`);
+  return $card;
+}
+
+function mkProductsGrid(products) {
+  const $containerEl = $('<div class="container-fluid"></div>');
+  const $rowEl = $('<div class="row"></div>');
+  $containerEl.append($rowEl);
+  products.forEach((product) => {
+    const $colEl = $('<div class="col-md-4"></div>');
+    $colEl.append(mkProductCard(product));
+    $rowEl.append($colEl);
+  });
+  return $containerEl;
+}
 
 $(() => {
+  $('#root').append(navbarTemplate);
   $.ajax('./static/categories.json')
     .done((categories) => {
       const $carousel = mkCarousel(categories);
@@ -21,5 +42,9 @@ $(() => {
       </li>`);
       });
     });
-  $('#root').append(navbarTemplate);
+  $.ajax('./static/products.json')
+    .done((products) => {
+      const $productsGrid = mkProductsGrid(products);
+      $('#root').append($productsGrid);
+    });
 });
